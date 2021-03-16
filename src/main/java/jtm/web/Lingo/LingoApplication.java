23e711f5675cae69fd3448beba3004a140d2cc10
UnofficialHenry <54036782+UnofficialHenry@users.noreply.github.com>
2021-03-16 10:05:43 +0200

    Add files via upload

---
+++ src/main/java/jtm/web/Lingo/LingoApplication.java
@@ -0,0 +1,13 @@
+package jtm.web.Lingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jtm/web/Lingo/LingoApplicationTests.java
@@ -0,0 +1,13 @@
+package jtm.web.Lingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
