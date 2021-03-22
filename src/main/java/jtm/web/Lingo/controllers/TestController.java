727a2a1e127b1be67cd34fe218d0f1f937629205
Oskars Martinsons <oskars.martinsons@inbox.lv>
2021-03-22 22:11:40 +0200

    with pointCounter and highscore

--- src/main/java/jtm/web/Lingo/controllers/TestController.java
+++ src/main/java/jtm/web/Lingo/controllers/TestController.java
@@ -16,7 +16,11 @@ public class TestController {
 	private String gameWord = null;
 	private int guessCount=0;
 	private int roundCount=1;
+	private int pointCount=0;
+	private int highScore=0;
+	private int[] levels = {100, 80, 60, 45, 30};
 	private String[] words= new String[6];
+	// private String[][] colorMatrix = new String[4] [4];
 
 	@Autowired
 	private RandWordUtil randWordUtil;
@@ -30,6 +34,8 @@ public class TestController {
 	String inGame(@RequestParam(value="word",defaultValue = "     ",required = true)String word, Model model) {
 		model.addAttribute("submit","SUBMIT");
 		model.addAttribute("roundNumber",roundCount);
+		model.addAttribute("totalPoints", pointCount);
+		model.addAttribute("highscore", highScore);
 		if (gameWord == null)
 			gameWord = randWordUtil.getRandomWord();
 		String word1 = gameWord;
@@ -38,12 +44,14 @@ public class TestController {
 		for(int i = 0; i < 5; i++) {
 			model.addAttribute("classitem"+Integer.toString(guessCount)+Integer.toString(i + 1),colors[i]);//assigns color to cell
 			model.addAttribute("cell"+Integer.toString(guessCount)+Integer.toString(i + 1),word.charAt(i));//assigns input letters to cell
+	//		colorMatrix [guessCount] [i] = colors [i];          // save colors [i] in matrix where [guessCount] is row nr and [i] colon nr
 		}
 		words[guessCount]=word;
 		if(guessCount>0) {
 			for(int i=1;i<guessCount;i++) {
 				for(int j=0;j<5;j++) {
 					model.addAttribute("cell"+Integer.toString(guessCount-i)+Integer.toString(j + 1),words[guessCount-i].charAt(j));
+	//  			model.addAttribute("classitem"+Integer.toString(guessCount-i)+Integer.toString(j + 1), colorMatrix[guessCount-i] [j]);  // shows matrix colors to old guesses
 				}
 			}
 		}
@@ -51,14 +59,20 @@ public class TestController {
 			model.addAttribute("cell11",word1.charAt(0));
 		guessCount++;
 		if(word1.toUpperCase().equals(word)) {
+			pointCount = pointCount + levels [guessCount-2];
 			gameWord=null;
 			guessCount=0;
 			roundCount++;
+			
 			words=new String[6];
 			model.addAttribute("submit","NEXT ROUND");
 		}
-		if(guessCount>5)
+		if(guessCount>5) {
+			if (highScore < pointCount) {
+				highScore = pointCount;
+			}
 			return endGame();
+		}
 		return "in_game";
 	}
 	
@@ -67,6 +81,7 @@ public class TestController {
 		guessCount=0;
 		roundCount=1;
 		gameWord=null;
+		pointCount= 0;
 		words=new String[6];
 		return "end_game";
 	}
--- src/test/java/jtm/web/Lingo/LingoApplicationTests.java
+++ src/test/java/jtm/web/Lingo/LingoApplicationTests.java
@@ -9,5 +9,24 @@ class LingoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	/*	
+	@Test
+	public void checkLength() {
+	    int expected = 5 ;
+	    String input = "word";
+	    int actual = input.length();
+	    assertEquals("Word should be with 5 letters", expected, actual);
+	}
+	
+	@Test
+	public void checkLetters() {
+	    boolean expected = true ;
+	    boolean actual;
+	    for (int i = 0; i<5; i++) {
+	   		if ( ! input.charAt(i).matches(".*[^A-Z].*") ) { 
+	       	actual = false;
+	    }
+	    assertEquals("Word should contain letters A-Z", expected, actual);
+	} */
 
 }
